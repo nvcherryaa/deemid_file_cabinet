2e0d98382b3feb8d8ba6ff48ddb9699ba95d91f6
deemid <nirvanacherryaa@gmail.com>
2016-05-29 02:10:51 +0800

    access control allow origin

--- app/controllers/api/fc_controller.rb
+++ app/controllers/api/fc_controller.rb
@@ -1,5 +1,12 @@
 class Api::FcController < ApplicationController
+  after_action :set_access_control_headers
+
   def index
     render json: Doc.all
+  end
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
   end
 end
